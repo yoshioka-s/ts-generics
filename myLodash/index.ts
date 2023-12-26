f7f98826e5a3780f743b8aece64236b8916db45a
yoshioka-s <yoshioka63@gmail.com>
2023-12-26 16:39:54 +0900

    feat: add question

--- myLodash/index.ts
+++ myLodash/index.ts
@@ -1,6 +1,5 @@
-type Each = {
-  <T>(items: T[], f: (item: T, index: number) => void): void
-}
+type Each = <T>(items: T[], f: (item: T, index: number) => void) => void
+
 const each: Each = (items, callback) => {
   for (let index = 0; index < items.length; index++) {
     const item = items[index];
@@ -8,6 +7,32 @@ const each: Each = (items, callback) => {
   }
 }
 
-export default {
-  each
+type Map = /* 定義せよ */
+const map: Map = (items, callback) => {
+  const result = []
+  for (let index = 0; index < items.length; index++) {
+    const item = items[index]
+    result.push(callback(item, index))
+  }
+  return result
+}
+console.log(map(['a', 'b'], (e) => e.length)[0].toPrecision())
+console.log(map([1, false], (e) => Boolean(e))[0].valueOf())
+
+type Find = /* 定義せよ */
+const find: Find = (items, callback) => {
+  for (let index = 0; index < items.length; index++) {
+    const item = items[index]
+    if (callback(item, index)) {
+      return item
+    }
+  }
+}
+console.log(find(['a', 'b'], (e) => e.length === 1))
+console.log(find([1, false], (e) => Boolean(e)))
+
+export {
+  each,
+  map,
+  find,
 }
--- myLodash/js/index.js
+++ myLodash/js/index.js
@@ -5,6 +5,30 @@ const each = (items, callback) => {
   }
 }
 
+const map = (items, callback) => {
+  const result = []
+  for (let index = 0; index < items.length; index++) {
+    const item = items[index]
+    result.push(callback(item, index))
+  }
+  return result
+}
+console.log(map(['a', 'b'], (e) => e.length)[0].toPrecision())
+console.log(map([1, false], (e) => Boolean(e))[0].valueOf())
+
+const find = (items, callback) => {
+  for (let index = 0; index < items.length; index++) {
+    const item = items[index]
+    if (callback(item, index)) {
+      return item
+    }
+  }
+}
+console.log(find(['a', 'b'], (e) => e.length === 1))
+console.log(find([1, false], (e) => Boolean(e)))
+
 export {
-  each
+  each,
+  map,
+  find,
 }
